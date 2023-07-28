623c080c14f06808e3cee20316f3ae66b4d7cfe7
WalterjDm <walterjdoblado569@unitec.edu>
2023-07-28 13:10:38 -0600

    primer commit

---
+++ Lab2P2_walterdoblado/src/lab2p2_walterdoblado/Lab2P2_walterdoblado.java
@@ -0,0 +1,50 @@
+
+package lab2p2_walterdoblado;
+
+import java.util.Scanner;
+
+
+public class Lab2P2_walterdoblado {
+
+    
+    public static void main(String[] args) {
+     Scanner lea = new Scanner(System.in);
+     int opc = 0;
+        while(opc !=2){
+            
+            
+            if (opc==1) {
+                
+                
+                
+                
+                
+            }else if(opc==2){
+                
+            }else{
+                
+                
+            }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        }
+     
+     
+        
+        
+        
+        
+        
+        
+    }
+    
+}
